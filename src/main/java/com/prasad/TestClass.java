1876d49da50d9e405dadb905111adfb35eedc5a3
prasad <abc_123@gmail.com>
2017-08-06 19:57:58 -0500

    first commit

---
+++ src/main/java/com/prasad/TestClass.java
@@ -0,0 +1,11 @@
+package com.prasad;
+public class TestClass
+{
+	public static void main(String args[])
+	{
+		System.out.println("This is my first ubuntu in java ");
+		String abc="prasad";
+		System.out.println("output:"+abc);
+	}
+
+}
